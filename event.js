185be1dd948808b43be87fcc4a968a0a708cbc51
jeannotds <diambujeannot@gmail.com>
2022-05-24 16:58:31 +0100

    Amelioration de progress auto et boutton suivant

--- event.js
+++ event.js
@@ -16,10 +16,12 @@ let suivant12 = document.querySelector('#suivant-12');
 let suivant13 = document.querySelector('#suivant-13');
 let suivant14 = document.querySelector('#suivant-14');
 let suivant15 = document.querySelector('#suivant-15');
+console.log(suivant15);
 const btnStart = document.querySelector('.btn-start');
 const timeSpan = document.querySelectorAll('.time');
 const progressBar = document.querySelectorAll('.progress-inner');
 
+action();
 function action(){
     commencer.addEventListener('click', (e)=>{
         e.preventDefault();
@@ -36,178 +38,181 @@ function action(){
             console.log("commencer")
         }
         else{
-            function index1(){
-                e.preventDefault();
-                index.style.display = "none";
-                afQuest[1].style.display ="block";
-                            //SUIVANT1
-                    suivant1.addEventListener('click', (e)=>{
+        e.preventDefault()
+        index.style.display = "none";
+        afQuest[1].style.display ="block";
+        let interval = 60;
+                var countDown = setInterval(()=>{
+                    interval--;
+                    let progressWidth = interval / 60*100;
+                    progressBar[0].style.width = progressWidth++ +"%";
+                     timeSpan[0].innerHTML = interval + "s";
+                     if (interval == 0) {
                         afQuest[1].style.display ="none";
                         afQuest[2].style.display ="block";
-                        e.preventDefault();
-                        console.log('cool')  
+                     }
+                   
+                }, 1000)
+                suivant1.addEventListener('click', (e)=>{
+                        afQuest[1].style.display ="none";
+                        afQuest[2].style.display ="block";
                 })
-                function count1(){
-                    addEventListener('click', (e)=>{
-                        e.preventDefault();
-                        console.log('cool')
-                        let interval = 60;
-                        var countDown = setInterval(()=>{
-                            interval--;
-                            let progressWidth = interval / 60*100;
-                            if(interval >= 0){
-                                progressBar[0].style.width = progressWidth++ +"%";
-                                timeSpan[0].innerHTML = interval + "s";
-                                checkColor(progressWidth)
-                            }else{
-                                clearInterval(countDown)
-                                timeSpan[0].innerHTML = "0s";
-                                if(timeSpan[0].innerHTML = "0s"){
-                                    afQuest[1].style.display ="none";
-                                    afQuest[2].style.display ="block";
-                                    //SUIVANT 2
-                                    suivant2.addEventListener('click', (e)=>{
-                                        console.log("ddddddddd")
-                                        function count2(){
-                                            
-                                        }
-                                        count2();
-                                    })
-                                }
-                            }
-                        }, 1000)
-                    })
-                    const checkColor = (width) =>{
-                        if(width>60){
-                            progressBar[0].style.background = "green";
-                        }
-                    }
-                }
-                count1();
-            }
-            index1();
-
-
-
-
-
-
-
-
-            // clearInterval(countDown)
-            //                     timeSpan[0].innerHTML = "0s";
-            //                     if(timeSpan[0].innerHTML = "0s"){
-            //                         afQuest[1].style.display ="none";
-            //                         afQuest[2].style.display ="block";
-            //                     }
-
-            
-        // e.preventDefault()
-        // index.style.display = "none";
-        // afQuest[1].style.display ="block";
-
-            
-        // e.preventDefault()
-        // index.style.display = "none";
-        // afQuest[1].style.display ="block";
-        // count1()
+    
+        // BOUTTON SUIVANT1
         
-        // // BOUTTON SUIVANT1
-        // suivant1.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[1].style.display ="none";
-        //     afQuest[2].style.display ="block";
-        //     count2();
-        // })
-        //  // BOUTTON SUIVANT2
-        //  suivant2.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[2].style.display ="none";
-        //     afQuest[3].style.display ="block";
-        //     count3();   
-        // })
-        // suivant3.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[3].style.display ="none";
-        //     afQuest[4].style.display ="block";
-        //     count4();   
-        // })
-        // suivant4.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[4].style.display ="none";
-        //     afQuest[5].style.display ="block";
-        //     count5();   
-        // })
-        // suivant5.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[5].style.display ="none";
-        //     afQuest[6].style.display ="block";
-        //     count6();   
-        // })
-        // suivant6.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[6].style.display ="none";
-        //     afQuest[7].style.display ="block";
-        //     count7();   
-        // })
-        // suivant7.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[7].style.display ="none";
-        //     afQuest[8].style.display ="block";
-        //     count8();   
-        // })
-        // suivant8.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[8].style.display ="none";
-        //     afQuest[9].style.display ="block";
-        //     count9();   
-        // })
-        // suivant9.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[9].style.display ="none";
-        //     afQuest[10].style.display ="block";
-        //     count10();   
-        // })
-        // suivant10.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[10].style.display ="none";
-        //     afQuest[11].style.display ="block";
-        //     count11();   
-        // })
-        // suivant11.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[11].style.display ="none";
-        //     afQuest[12].style.display ="block";
-        //     count12();   
-        // })
-        // suivant12.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[12].style.display ="none";
-        //     afQuest[13].style.display ="block";
-        //     count13();   
-        // })
-        // suivant13.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[13].style.display ="none";
-        //     afQuest[14].style.display ="block";
-        //     count14();   
-        // })
-        // suivant14.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[14].style.display ="none";
-        //     afQuest[15].style.display ="block";
-        //     count15();   
-        // })
-        // suivant14.addEventListener('click', (e)=>{
-        //     e.preventDefault();
-        //     afQuest[14].style.display ="none";
-        //     afQuest[15].style.display ="block";
-        //     count15();   
-        // })
         nom = document.querySelector('#nom').value ="";
         email = document.querySelector('#email').value="";
         } 
     })
 }
+
 // ########################################################################################
-action();
+      function suivant_1(){
+        suivant1.addEventListener('click', (e)=>{
+            e.preventDefault();
+            afQuest[1].style.display ="none";
+            afQuest[2].style.display ="block";
+            let interval = 60;
+                var countDown = setInterval(()=>{
+                    interval--;
+                    let progressWidth = interval / 60*100;
+                    progressBar[1].style.width = progressWidth++ +"%";
+                     timeSpan[1].innerHTML = interval + "s";
+                     if (interval == 0) {
+                        afQuest[2].style.display ="none";
+                        afQuest[3].style.display ="block";
+                     }
+                   
+                }, 1000)
+                suivant1.addEventListener('click', (e)=>{
+                        afQuest[2].style.display ="none";
+                        afQuest[3].style.display ="block";
+                })
+        })
+      }
+      suivant_1()
+        function suivant_2(){
+            suivant2.addEventListener('click', (e)=>{
+                e.preventDefault();
+                afQuest[2].style.display ="none";
+                afQuest[3].style.display ="block";
+                // count3();   
+            })
+         }
+         suivant_2()
+       function suivant_3(){
+        suivant3.addEventListener('click', (e)=>{
+            e.preventDefault();
+            afQuest[3].style.display ="none";
+            afQuest[4].style.display ="block";
+            //count4();   
+        })
+       }
+       suivant_3()
+       function suivant_4(){
+        suivant4.addEventListener('click', (e)=>{
+            e.preventDefault();
+            afQuest[4].style.display ="none";
+            afQuest[5].style.display ="block";
+            // count5();   
+        })
+       }
+       suivant_4()
+        function suivant_5(){
+            suivant5.addEventListener('click', (e)=>{
+                e.preventDefault();
+                afQuest[5].style.display ="none";
+                afQuest[6].style.display ="block";
+                // count6();   
+            })
+        }
+        suivant_5()
+       function suivant_6(){
+        suivant6.addEventListener('click', (e)=>{
+            e.preventDefault();
+            afQuest[6].style.display ="none";
+            afQuest[7].style.display ="block";
+            // count7();   
+        })
+       }
+       suivant_6()
+       function suivant_7(){
+        suivant7.addEventListener('click', (e)=>{
+            e.preventDefault();
+            afQuest[7].style.display ="none";
+            afQuest[8].style.display ="block";
+            // count8();   
+        })
+       }
+       suivant_7()
+        function suivant_8(){
+            suivant8.addEventListener('click', (e)=>{
+                e.preventDefault();
+                afQuest[8].style.display ="none";
+                afQuest[9].style.display ="block";
+                // count9();   
+            })
+        }
+        suivant_8()
+        function suivant_9(){
+            suivant9.addEventListener('click', (e)=>{
+                e.preventDefault();
+                afQuest[9].style.display ="none";
+                afQuest[10].style.display ="block";
+                // count10();   
+            })
+        }
+        suivant_9()
+        function suivant_10(){
+            suivant10.addEventListener('click', (e)=>{
+                e.preventDefault();
+                afQuest[10].style.display ="none";
+                afQuest[11].style.display ="block";
+                // count11();   
+            })
+        }
+        suivant_10()
+        function suivant_11(){
+            suivant11.addEventListener('click', (e)=>{
+                e.preventDefault();
+                afQuest[11].style.display ="none";
+                afQuest[12].style.display ="block";
+                // count12();   
+            })
+        }
+        suivant_11()
+       function suivant_12(){
+        suivant12.addEventListener('click', (e)=>{
+            e.preventDefault();
+            afQuest[12].style.display ="none";
+            afQuest[13].style.display ="block";
+            // count13();   
+        })
+       }
+       suivant_12()
+        function suivant_13(){
+            suivant13.addEventListener('click', (e)=>{
+                e.preventDefault();
+                afQuest[13].style.display ="none";
+                afQuest[14].style.display ="block";
+                // count14();   
+            })
+        }
+        suivant_13()
+      function suivant_14(){
+        suivant14.addEventListener('click', (e)=>{
+            e.preventDefault();
+            afQuest[14].style.display ="none";
+            afQuest[15].style.display ="block";
+            // count15();   
+        })
+      }
+     function suivant_15(){
+        suivant15.addEventListener('click', (e)=>{
+            e.preventDefault();
+            afQuest[15].style.display ="none";
+            // afQuest[15].style.display ="block";
+            // count15();   
+        })
+     }
+     suivant_15()
--- garde.js
+++ garde.js
@@ -100,3 +100,139 @@ function count1(){
         }
     }
 }
+
+
+
+// function suivant_1(){
+//     suivant1.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[1].style.display ="none";
+//         afQuest[2].style.display ="block";
+//         count2();
+//     })
+// }
+//  // BOUTTON SUIVANT2
+// function suivant_2(){
+//     suivant2.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[2].style.display ="none";
+//         afQuest[3].style.display ="block";
+//         count3();   
+//     })
+// }
+
+// function suivant_3(){
+//     suivant3.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[3].style.display ="none";
+//         afQuest[4].style.display ="block";
+//         count4();   
+//     })
+// }
+
+// function suivant_4(){
+//     suivant4.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[4].style.display ="none";
+//         afQuest[5].style.display ="block";
+//         count5();   
+//     })
+// }
+
+// function suivant_5(){
+//     suivant5.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[5].style.display ="none";
+//         afQuest[6].style.display ="block";
+//         count6();   
+//     })
+// }
+
+// function suivant_6(){
+//     suivant6.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[6].style.display ="none";
+//         afQuest[7].style.display ="block";
+//         count7();   
+//     })
+// }
+
+// function suivant_7(){
+//     suivant7.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[7].style.display ="none";
+//         afQuest[8].style.display ="block";
+//         count8();   
+//     })
+// }
+
+// function suivant_8(){
+//     suivant8.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[8].style.display ="none";
+//         afQuest[9].style.display ="block";
+//         count9();   
+//     })
+// }
+
+// function suivant_9(){
+//     suivant9.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[9].style.display ="none";
+//         afQuest[10].style.display ="block";
+//         count10();   
+//     })
+// }
+
+// function suivant_10(){
+//     suivant10.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[10].style.display ="none";
+//         afQuest[11].style.display ="block";
+//         count11();   
+//     })
+// }
+
+// function suivant_11(){
+//     suivant11.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[11].style.display ="none";
+//         afQuest[12].style.display ="block";
+//         count12();   
+//     })
+// }
+
+// function suivant_12(){
+//     suivant12.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[12].style.display ="none";
+//         afQuest[13].style.display ="block";
+//         count13();   
+//     })
+// }
+
+// function suivant_13(){
+//     suivant13.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[13].style.display ="none";
+//         afQuest[14].style.display ="block";
+//         count14();   
+//     })
+// }
+
+// function suivant_14(){
+//     suivant14.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[14].style.display ="none";
+//         afQuest[15].style.display ="block";
+//         count15();   
+//     })
+// }
+
+// function suivant_15(){
+//     suivant14.addEventListener('click', (e)=>{
+//         e.preventDefault();
+//         afQuest[14].style.display ="none";
+//         afQuest[15].style.display ="block";
+//         count15();   
+//     })
